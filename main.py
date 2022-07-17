0909acc22f4b4b9f39cde49d46d99c6f4780d305
JVN313 <82493100+JVN313@users.noreply.github.com>
2022-07-17 10:13:01 -0400

    Fixed indexing issue and added enumerate to Card-Creator

--- main.py
+++ main.py
@@ -4,21 +4,20 @@ from classes import *
 Cards = []
 
 def Card_Creator():
-    Card_Names= ["ONE","TWO","THREE","FOUR","FIVE","SIX","SEVEN","EIGHT","NINE","TEN",]
-    Card_Values = 1
-    for name in Card_Names:
-        Cards.append(Card(Card_Values,name))
-        Card_Values += 1
+    Card_Names= ["ONE","TWO","THREE","FOUR","FIVE","SIX","SEVEN","EIGHT","NINE","TEN"]
+    for index, name in enumerate(Card_Names, 1):
+        Cards.append(Card(index,name))
 
 Card_Creator()
 
 def Draw():
-    return Cards.pop(random.randint(0,len(Cards)))
+    return Cards.pop(random.randint(0,len(Cards)-1))
 
 comp_pick = Draw()
 player_pick = Draw()
+test = len(Cards)
 player_wins = 0
-comp_wins =0
+comp_wins = 0
 playing = True
 
 def Re_Draw():
@@ -36,29 +35,20 @@ while playing:
         Results()
         print()
         print("Comp Win")
+        print()
     elif player_pick.value > comp_pick.value:
         player_wins += 1
         Results()
         print()
         print("Player Win")
+        print()
 
-    Re_Draw()
-
-    #elif comp_pick.value == player_pick.value:
-    #    comp_pick= Draw()
-    
-    if len(Cards) == 0:
+    if Cards != []:
+        Re_Draw() 
+    else:
         playing = False
         print()
         print(f"Player Wins: {player_wins}\nComp Wins: {comp_wins}")
-
-    #replay = input("PLay Again?: ")
-    #if replay == "y":
-    #    print()
-    #    comp_pick= Draw()
-    #    player_pick = Draw()
-    #else:
-    #    playing = False
 
 
 # TODO: Fix Draw() on line 16, so the random number generated will always be in range for indexing.
