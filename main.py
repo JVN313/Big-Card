87911b42003ab79b077a16e081cbdd9cbc21415e
JVN313 <82493100+JVN313@users.noreply.github.com>
2022-07-20 12:24:17 -0400

    added timer to give space between draws

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+import time
 from classes import *
 
 Cards = []
@@ -15,7 +16,6 @@ def Draw():
 
 comp_pick = Draw()
 player_pick = Draw()
-test = len(Cards)
 player_wins = 0
 comp_wins = 0
 playing = True
@@ -23,13 +23,14 @@ playing = True
 def Re_Draw():
     global comp_pick, player_pick
     comp_pick = Draw()
-    player_pick = Draw()
+    player_pick = Draw()  
 
 def Results():
     print(f"Comp: {comp_pick.name}")
     print(f"Player: {player_pick.name}")
 
 while playing:
+    time.sleep(2)
     if comp_pick.value > player_pick.value:
         comp_wins += 1
         Results()
@@ -50,9 +51,9 @@ while playing:
         print()
         print(f"Player Wins: {player_wins}\nComp Wins: {comp_wins}")
         if comp_wins > player_wins:
-            print("Comp Won The Game Sorry :(")
+            print("Comp Won The Game Sorry :(\n")
         else:
-            print("You Won The Game Congrats!")
+            print("You Won The Game Congrats!\n")
 
 
-# TODO: Add timer so code runs slower ach loop cycle
+# TODO: find ways to add player interactivity ex. Replay Button
